08e31bf2cea0283e1c1ba55847f488679a901e3e
polegithub <tschengkong@163.com>
2017-02-19 16:47:09 +0800

    add swift project

---
+++ SwiftMachineLibrary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftMachineLibrary
+//
+//  Created by poleness on 2017/2/19.
+//  Copyright © 2017年 poleness. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
